eb8237828c0177ae6822d77f7c0e92027c78aa26
hermanjd <herman.dragland@gmail.com>
2018-10-16 18:09:59 +0200

    Added FeedActivity
    FeedActivity added

---
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/FeedActivity.java
@@ -0,0 +1,13 @@
+package no.hiof.android2018.gruppe11.shrooms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
