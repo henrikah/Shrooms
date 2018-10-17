1ec295cd6702d473a01f3210bef5a12ea0e7abbc
hermanjd <herman.dragland@gmail.com>
2018-10-17 13:35:44 +0200

    Removed Actionbar in splashscreen

--- app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MainActivity.java
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MainActivity.java
@@ -1,13 +1,24 @@
 package no.hiof.android2018.gruppe11.shrooms;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Gjemmer Actionbaren:
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
