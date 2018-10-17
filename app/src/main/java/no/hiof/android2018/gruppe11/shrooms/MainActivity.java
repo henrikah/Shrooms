e3ec86708979e5ad6cb2ca2fdfc75b87a08f3604
MariusMOlsen <mariusol@hiof.no>
2018-10-18 01:16:40 +0200

    Update app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MainActivity.java

--- app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MainActivity.java
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MainActivity.java
@@ -1,10 +1,18 @@
 package no.hiof.android2018.gruppe11.shrooms;
 
+<<<<<<< HEAD
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+=======
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+>>>>>>> MariusBranch
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> MariusBranch
     }
 }
