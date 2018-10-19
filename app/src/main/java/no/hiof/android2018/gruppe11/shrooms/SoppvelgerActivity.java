0553323eed63741f2ecc3f8f930912780142f13a
MariusMOlsen <mariusol@hiof.no>
2018-10-19 21:33:41 +0200

    kk

--- app/src/main/java/no/hiof/android2018/gruppe11/shrooms/SoppvelgerActivity.java
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/SoppvelgerActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -14,44 +15,53 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-public class SoppvelgerActivity extends Activity {
-
+public class SoppVelgerActivity extends Activity {
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soppvelger);
-        collectSopp();
-        initRecyclerView();
+        setContentView(R.layout.activity_sopp_velger);
+
+
+  /*  new Sopp("Fluesopp", "fff", "Fin og rød", null, true);
+    new Sopp("Fotsopp", "ffddf", "Klør veldig", null, true);
+    new Sopp("Underlivsopp", "ffddf", "Lukter vondt", null, true);
+    */
+    retrieveSopp();
+
     }
 
-    private void collectSopp(){
-        FirebaseFirestore fb = FirebaseFirestore.getInstance();
-        CollectionReference soppcollection = fb.collection("Shrooms");
-        Query soppQuery = soppcollection;
+
+    private void initSoppRecycler() {
+        recyclerView = findViewById(R.id.soppRecycler);
+        recyclerView.setAdapter(new SoppRecyclerAdapter(this, Sopp.getSoppListe()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+
+    private void retrieveSopp(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference soppColletionRef = db.collection("Shrooms");
+
+        Query soppQuery = soppColletionRef;
 
         soppQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
-                        Sopp sopp = new Sopp(documentSnapshot.getId(),
-                                            documentSnapshot.get("navn").toString(),
-                                            documentSnapshot.get("info").toString());
+                if (task.isSuccessful()) {
+                    for(QueryDocumentSnapshot document: task.getResult()){
+                        //Toast.makeText(SoppVelgerActivity.this, (String)document.get("navn"), Toast.LENGTH_SHORT).show();
+                        Sopp sopp = new Sopp((String) document.get("navn"), "fff", (String) document.get("info"), null, false);
                     }
-
                 }
                 else {
-
+                    Toast.makeText(SoppVelgerActivity.this, "Nei", Toast.LENGTH_SHORT).show();
                 }
+
+                // Starter sopprecycleren etter at soppene er instansierte
+                initSoppRecycler();
             }
         });
     }
-
-    private void initRecyclerView(){
-        RecyclerView recyclerView = findViewById(R.id.mainFeed);
-        SoppRecyclerAdapter adapter = new SoppRecyclerAdapter(this, Sopp.getSoppListe());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
-
 }
