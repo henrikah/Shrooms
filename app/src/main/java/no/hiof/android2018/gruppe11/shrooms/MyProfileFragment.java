1b42059c4b10a2e16fb2f04a2dadbd2bb62b5330
LarsMars <lars.r.johansen@gmail.com>
2018-11-19 04:24:10 +0100

    Halvferdig profiloppsett

--- app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MyProfileFragment.java
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MyProfileFragment.java
@@ -3,9 +3,12 @@ package no.hiof.android2018.gruppe11.shrooms;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +19,10 @@ public class MyProfileFragment extends Fragment {
     private TextView text;
     private FirebaseAuth mAuth;
     FirebaseUser currentUser;
+    FragmentTransaction fragmentTransaction;
+    Fragment fragment;
+    ImageButton infoButton;
+    ImageButton postsButton;
 
 
     @Override
@@ -23,6 +30,7 @@ public class MyProfileFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+
     }
 
     @Override
@@ -34,8 +42,33 @@ public class MyProfileFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        text = getView().findViewById(R.id.myProfileText);
-        text.setText(currentUser.getEmail());
+        //text = getView().findViewById(R.id.myProfileText);
+        //text.setText(currentUser.getEmail());
+
+        fragment = new MyInfoFragment();
+        fragmentTransaction = this.getChildFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.myLayout, fragment).commit();
+
+
+       postsButton = getView().findViewById(R.id.infoImageButton);
+       postsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new MyPostFragment();
+                fragmentTransaction = getChildFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.myLayout, fragment).commit();
+            }
+        });
+
+        infoButton = getView().findViewById(R.id.postsImageButton);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new MyInfoFragment();
+                fragmentTransaction = getChildFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.myLayout, fragment).commit();
+            }
+        });
 
     }
 }
