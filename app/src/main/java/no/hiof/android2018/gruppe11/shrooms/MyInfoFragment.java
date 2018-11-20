ab9072dc958b7adebb0efca9a9d952c7b0653565
LarsMars <lars.r.johansen@gmail.com>
2018-11-20 20:28:43 +0100

    Profil på vei

--- app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MyInfoFragment.java
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/MyInfoFragment.java
@@ -1,15 +1,34 @@
 package no.hiof.android2018.gruppe11.shrooms;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class MyInfoFragment extends Fragment {
+    private Image image;
+    private TextView firstName;
+    private TextView lastName;
+    private TextView mail;
+    private FirebaseFirestore db;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     public MyInfoFragment() {
         // Required empty public constructor
@@ -22,16 +41,36 @@ public class MyInfoFragment extends Fragment {
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_myinfo, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
+        lastName = getView().findViewById(R.id.lastName);
+        firstName = getView().findViewById(R.id.firstName);
+        mail = getView().findViewById(R.id.userMail);
 
+        db.collection("Bruker").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        lastName.setText(document.getString("Lastname"));
+                        firstName.setText(document.getString("Firstname"));
+                        mail.setText(document.getString("Mail"));
+                    }
+                } else {
 
-
+                }
+            }
+        });
+    }
 }
