8c284c8c2745b5ebd01741d93da605e52757fb30
Henrik Helgesen <henrik.helgesen@gmail.com>
2018-11-26 11:23:22 +0100

    CreateUser 100% ferdig.

--- app/src/main/java/no/hiof/android2018/gruppe11/shrooms/adapter/MushroomAdapter.java
+++ app/src/main/java/no/hiof/android2018/gruppe11/shrooms/adapter/MushroomAdapter.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,11 +17,9 @@ import no.hiof.android2018.gruppe11.shrooms.firebase.schema.ShroomSchema;
 public class MushroomAdapter extends ArrayAdapter {
 
     private ArrayList<ShroomSchema> arrayList;
-    private Context context;
     public MushroomAdapter(@NonNull Context context, ArrayList<ShroomSchema> arrayList) {
         super(context, R.layout.layout_mushroom_type_item);
         this.arrayList = arrayList;
-        this.context = context;
     }
 
     @Override
@@ -37,16 +34,12 @@ public class MushroomAdapter extends ArrayAdapter {
         if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = mInflater.inflate(R.layout.layout_mushroom_type_item, parent, false);
-        //    mViewHolder.mFlag = convertView.findViewById(R.id.ivFlag);
             mViewHolder.mName = convertView.findViewById(R.id.tvName);
-        //    mViewHolder.mPopulation = convertView.findViewById(R.id.tvPopulation);
             convertView.setTag(mViewHolder);
         } else {
             mViewHolder = (ViewHolder) convertView.getTag();
         }
-        //mViewHolder.mFlag.setImageResource(arrayList.get(position));
         mViewHolder.mName.setText(arrayList.get(position).getName());
-        //mViewHolder.mPopulation.setText(arrayList.get(position).getDescription());
 
         return convertView;
     }
@@ -56,8 +49,6 @@ public class MushroomAdapter extends ArrayAdapter {
         return getView(position, convertView, parent);
     }
     private static class ViewHolder {
-        ImageView mFlag;
         TextView mName;
-        TextView mPopulation;
     }
 }
